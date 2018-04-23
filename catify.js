2bf812397e171776ecb9cd0a611d6b63559d92b6
Julia <jnmareike@gmail.com>
2018-04-24 10:19:18 +1200

    replaces some thumbnails w one cat

--- catify.js
+++ catify.js
@@ -1,5 +1,15 @@
 (() => {
 
+  const images = document.getElementsByTagName('img')
+  catify(images)
+
+  function catify (images) {
+    for (let i = 0; i < images.length; i++) {
+      if (images[i].id === 'img')
+      images[i].src = "https://i.ytimg.com/vi/rNSnfXl1ZjU/hqdefault.jpg?sqp=-oaymwEXCPYBEIoBSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLDhBrX2aiVwZ0Ig4zooNuTQTq4teQ"
+    }
+  }
+
 //                        A___A
 //            A___A       |o o|
 //      ____ / o o \      |='=|
