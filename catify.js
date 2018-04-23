8faee1394de9320eef71f82fda4ce69159111cd5
Julia <jnmareike@gmail.com>
2018-04-24 10:28:22 +1200

    hides some video titles

--- catify.js
+++ catify.js
@@ -1,12 +1,19 @@
 (() => {
 
   const images = document.getElementsByTagName('img')
-  catify(images)
+  const videos = document.getElementsByTagName('a')
 
   function catify (images) {
     for (let i = 0; i < images.length; i++) {
       if (images[i].id === 'img')
       images[i].src = "https://i.ytimg.com/vi/rNSnfXl1ZjU/hqdefault.jpg?sqp=-oaymwEXCPYBEIoBSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLDhBrX2aiVwZ0Ig4zooNuTQTq4teQ"
+    }
+  }
+
+  function hide (videos) {
+    for (let i = 0; i < videos.length; i++) {
+      if (videos[i].id === 'video-title')
+      videos[i].style.display = "none"
     }
   }
 
@@ -16,5 +23,8 @@
 // ___/~____   ='= /_____/    |_________
 //   (______)__m_m_)    /  ||||
 //                     |___||||]
+
+  catify(images)
+  hide(videos)
 
 })()
