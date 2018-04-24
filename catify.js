23ba58672d7d5a172c54e3d3b1336ff3d8ffb798
Julia <jnmareike@gmail.com>
2018-04-24 15:13:47 +1200

    es-linted

--- catify.js
+++ catify.js
@@ -1,30 +1,26 @@
 (() => {
-
   const images = document.getElementsByTagName('img')
   const videos = document.getElementsByTagName('a')
 
   function catify (images) {
     for (let cat of images) {
-      if (cat.id === 'img')
-      cat.src = "https://i.ytimg.com/vi/rNSnfXl1ZjU/hqdefault.jpg?sqp=-oaymwEXCPYBEIoBSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLDhBrX2aiVwZ0Ig4zooNuTQTq4teQ"
+      if (cat.id === 'img') { cat.src = 'https://i.ytimg.com/vi/rNSnfXl1ZjU/hqdefault.jpg?sqp=-oaymwEXCPYBEIoBSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLDhBrX2aiVwZ0Ig4zooNuTQTq4teQ' }
     }
   }
 
   function hide (videos) {
     for (let element of videos) {
-      if (element.id === 'video-title')
-      element.style.display = "none"
+      if (element.id === 'video-title') { element.style.display = 'none' }
     }
   }
 
-//                        A___A
-//            A___A       |o o|
-//      ____ / o o \      |='=|
-// ___/~____   ='= /_____/    |_________
-//   (______)__m_m_)    /  ||||
-//                     |___||||]
+  //                        A___A
+  //            A___A       |o o|
+  //      ____ / o o \      |='=|
+  // ___/~____   ='= /_____/    |_________
+  //   (______)__m_m_)    /  ||||
+  //                     |___||||]
 
   catify(images)
   hide(videos)
-
 })()
